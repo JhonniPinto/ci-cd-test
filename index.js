5ac10737462efb32d7519c5b8d8b1324d0cf67c5
JhonniPinto <jhonnipinto@gmail.com>
2019-11-17 19:07:28 -0300

    adding page2

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const pages = require('./controllers/pages')
 
 app.get('/', pages.home)
 app.get('/page1', pages.page1)
+app.get('/page2', pages.page2)
 app.get('/calc/:num1/:num2', pages.calc)
 
 app.listen(port, (err) => {
